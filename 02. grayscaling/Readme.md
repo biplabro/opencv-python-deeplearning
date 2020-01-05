8f5c43962fdfd765d984921f9e4129ee655dcc23
Biplab Roy <roy_biplab_89@yahoo.co.in>
2020-01-05 15:23:08 +0530

    readme added

---
+++ 02. grayscaling/Readme.md
@@ -0,0 +1,5 @@
+This example is of grayscaling images.
+Two alternate methods are shown for grayscaling
+
+One method is commented out. 
+Replace the ''' sections to use the alternate codes
--- 02. grayscaling/grayscaling.py
+++ 02. grayscaling/grayscaling.py
@@ -5,15 +5,29 @@ Created on Sun Jan  5 15:00:42 2020
 
 @author: roy
 """
+#Method-1
+#========
 
+#library import
 import cv2
+
+#load image
 image = cv2.imread('p02.jpg')
 cv2.imshow('color image', image)
 cv2.waitKey()
 
-
+#process image
 gray_image = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
 cv2.imshow('Grayscale', gray_image)
 cv2.waitKey()
 cv2.destroyAllWindows()
+
+#store image
 cv2.imwrite('Grayscaled.jpg', gray_image)
+
+#Method-2
+#========
+
+
+
+
