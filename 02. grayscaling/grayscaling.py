dc96bd619d5f33a92a86e0c640f889abcf49e6e7
Biplab Roy <roy_biplab_89@yahoo.co.in>
2020-01-05 18:38:15 +0530

    added alt method

--- 02. grayscaling/grayscaling.py
+++ 02. grayscaling/grayscaling.py
@@ -18,16 +18,23 @@ cv2.waitKey()
 
 #process image
 gray_image = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
-cv2.imshow('Grayscale', gray_image)
+cv2.imshow('B/W-1', gray_image)
 cv2.waitKey()
 cv2.destroyAllWindows()
 
 #store image
-cv2.imwrite('Grayscaled.jpg', gray_image)
+cv2.imwrite('Grayscale1.jpg', gray_image)
 
 #Method-2
 #========
 
+#load image as grayscale
+img = cv2.imread('p02.jpg',0)
 
+#show image
+cv2.imshow('B/W-2', img)
+cv2.waitKey()
+cv2.destroyAllWindows()
 
-
+#store image
+cv2.imwrite('Grayscale2.jpg', gray_image)
